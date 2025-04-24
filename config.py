d2fe67ee369444acaca2ae882b4202600e4234f4
vincent1go <isaievdmitro@icloud.com>
2025-04-24 12:32:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ WEBHOOK_URL = "https://telegram-pdf-bot-1f5c.onrender.com/telegram"
 # Путь к шаблонам PDF
 TEMPLATES = {
     "UR Recruitment LTD": "clean_template_no_text.pdf",
-    "SMALL WORLD RECRUITMENT LTD": "template_small_world.pdf"
+    "SMALL WORLD RECRUITMENT LTD": "template_small_world.pdf",
+    "MILA CONSULTANTS LTD": "contract_template3.pdf"
 }
